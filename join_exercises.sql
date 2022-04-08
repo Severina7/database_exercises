0bb29b0812bce7c91754d88d06f6f86890e94967
Arsene Severin Boundaone <arsene.boundaone@gmail.com>
2022-04-08 08:34:31 -0500

    adding temprary tables first exercise

--- join_exercises.sql
+++ join_exercises.sql
@@ -6,13 +6,47 @@ SELECT * FROM users LIMIT 1;
 # Use join, left join, and right join to combine results from the users and roles tables as we did in the lesson.
 # Before you run each query, guess the expected number of results.
 # Email column added for testing
-SELECT users.name AS users_name, users.email AS users_email, roles.name as role_name FROM users JOIN roles ON users.role_id = roles.id;
-SELECT users.name AS users_name, users.email AS users_email, roles.name as role_name FROM users LEFT JOIN roles ON users.role_id = roles.id;
-SELECT users.name AS users_name, users.email AS users_email, roles.name as role_name FROM users RIGHT JOIN roles ON users.role_id = roles.id;
+# Using JOIN (INNER JOIN)
+SELECT 
+    users.name AS users_name,
+    users.email AS users_email,
+    roles.name AS role_name
+FROM
+    users
+        JOIN
+    roles ON users.role_id = roles.id;
+    
+# Using LEFT JOIN
+SELECT 
+    users.name AS users_name,
+    users.email AS users_email,
+    roles.name AS role_name
+FROM
+    users
+        LEFT JOIN
+    roles ON users.role_id = roles.id;
+    
+# Using RIGHT JOIN
+SELECT 
+    users.name AS users_name,
+    users.email AS users_email,
+    roles.name AS role_name
+FROM
+    users
+        RIGHT JOIN
+    roles ON users.role_id = roles.id;
 # 3. Although not explicitly covered in the lesson, aggregate functions like count can be used with join queries.
 # Use count and the appropriate join type to get a list of roles along with the number of users that has the role.
 # Hint: You will also need to use group by in the query.USE employees;
-SELECT u.name AS UName, r.name as RName, COUNT(r.name) as 'Role Count' FROM users as u RIGHT JOIN roles as r ON u.role_id = r.id GROUP BY u.name, r.name, 'Role Count';
+SELECT 
+    u.name AS UName,
+    r.name AS RName,
+    COUNT(r.name) AS 'Role Count'
+FROM
+    users AS u
+        RIGHT JOIN
+    roles AS r ON u.role_id = r.id
+GROUP BY u.name , r.name , 'Role Count';
 
 # EMPLOYEES DATABASE
 
@@ -22,21 +56,52 @@ USE employees;
 SELECT * FROM employees;
 SELECT * FROM departments;
 SELECT * FROM dept_manager;
-SELECT d.dept_name as 'Department Name', (CONCAT (e.first_name, ' ', e.last_name)) as 'Manager Name' FROM employees as e JOIN dept_manager as dm ON e.emp_no = dm.emp_no JOIN departments as d ON d.dept_no = dm.dept_no ORDER BY dept_name;
+SELECT 
+    d.dept_name AS 'Department Name',
+    (CONCAT(e.first_name, ' ', e.last_name)) AS 'Manager Name'
+FROM
+    employees AS e
+        JOIN
+    dept_manager AS dm ON e.emp_no = dm.emp_no
+        JOIN
+    departments AS d ON d.dept_no = dm.dept_no
+ORDER BY dept_name;
 
 # 3. Find the name of all departments currently managed by women.
 USE employees;
 SELECT * FROM employees LIMIT 10;
 SELECT * FROM departments;
 SELECT * FROM dept_manager;
-SELECT d.dept_name as 'Department Name', (CONCAT (e.first_name, ' ', e.last_name)) as 'Manager Name', gender FROM employees as e JOIN dept_manager as dm ON e.emp_no = dm.emp_no AND to_date > NOW() AND gender = 'F' JOIN departments as d USING(dept_no) ORDER BY dept_name;
+SELECT 
+    d.dept_name AS 'Department Name',
+    (CONCAT(e.first_name, ' ', e.last_name)) AS 'Manager Name',
+    gender
+FROM
+    employees AS e
+        JOIN
+    dept_manager AS dm ON e.emp_no = dm.emp_no AND to_date > NOW()
+        AND gender = 'F'
+        JOIN
+    departments AS d USING (dept_no)
+ORDER BY dept_name;
 
 # 4. Find the current titles of employees currently working in the Customer Service department.
 USE employees;
 SELECT * FROM titles;
 SELECT * FROM departments;
 SELECT * FROM dept_emp;
-SELECT t.title, COUNT(de.emp_no) FROM dept_emp AS de JOIN titles AS t ON de.emp_no = t.emp_no AND t.to_date > NOW() AND de.to_date > NOW() JOIN departments AS d ON de.dept_no = d.dept_no AND d.dept_name = 'Customer Service' GROUP BY t.title;
+SELECT 
+    t.title, COUNT(de.emp_no)
+FROM
+    dept_emp AS de
+        JOIN
+    titles AS t ON de.emp_no = t.emp_no
+        AND t.to_date > NOW()
+        AND de.to_date > NOW()
+        JOIN
+    departments AS d ON de.dept_no = d.dept_no
+        AND d.dept_name = 'Customer Service'
+GROUP BY t.title;
 
 # 5. Find the current salary of all current managers.
 USE employees;
@@ -63,7 +128,14 @@ ORDER BY dept_name;
 # 6. Find the number of current employees in each department.
 SELECT * FROM dept_emp;
 SELECT	* FROM departments;
-SELECT	d.dept_no, d.dept_name, COUNT(emp_no) AS num_employees FROM dept_emp AS de JOIN departments AS d ON de.dept_no = d.dept_no AND de.to_date > CURDATE() GROUP BY dept_no, dept_name;
+SELECT 
+    d.dept_no, d.dept_name, COUNT(emp_no) AS num_employees
+FROM
+    dept_emp AS de
+        JOIN
+    departments AS d ON de.dept_no = d.dept_no
+        AND de.to_date > CURDATE()
+GROUP BY dept_no , dept_name;
 
 # 7. Which department has the highest average salary? Hint: Use current not historic information.
 SELECT * FROM dept_emp;
@@ -124,7 +196,18 @@ ORDER BY s.salary DESC
 LIMIT 1;
 
 # 10. Determine the average salary for each department. Use all salary information and round your results.
-SELECT d.dept_name, ROUND(AVG(s.salary),0) AS avg_dept_salary FROM departments d JOIN dept_emp de ON d.dept_no = de.dept_no JOIN employees e ON e.emp_no = de.emp_no JOIN salaries s ON e.emp_no = s.emp_no GROUP BY d.dept_name ORDER BY avg_dept_salary DESC;
+SELECT 
+    d.dept_name, ROUND(AVG(s.salary), 0) AS avg_dept_salary
+FROM
+    departments d
+        JOIN
+    dept_emp de ON d.dept_no = de.dept_no
+        JOIN
+    employees e ON e.emp_no = de.emp_no
+        JOIN
+    salaries s ON e.emp_no = s.emp_no
+GROUP BY d.dept_name
+ORDER BY avg_dept_salary DESC;
 
 # 11. Bonus: Find the names of all current employees, their department name, and their current manager's name.
 SELECT * FROM employees LIMIT 2;
@@ -132,7 +215,32 @@ SELECT * FROM dept_emp;
 SELECT * FROM dept_manager;
 SELECT * FROM departments;
 # Setting up the table with the managers' name because there is no table with managers' name on
-SELECT dm.dept_no, CONCAT(e.first_name, ' ', e.last_name) AS managers FROM employees AS e JOIN dept_manager AS dm ON e.emp_no = dm.emp_no AND to_date > NOW();
+SELECT 
+    dm.dept_no,
+    CONCAT(e.first_name, ' ', e.last_name) AS managers
+FROM
+    employees AS e
+        JOIN
+    dept_manager AS dm ON e.emp_no = dm.emp_no AND to_date > NOW();
 
 # Writing the final query to find the names of all current employees, their department name, and their current manager's name.
-SELECT CONCAT(e.first_name, ' ', e.last_name) AS 'Employee Name', d.dept_name AS 'Department Name', m.managers AS 'Manager Name' FROM employees AS e JOIN dept_emp AS de ON e.emp_no = de.emp_no AND de.to_date > NOW() JOIN departments AS d ON de.dept_no = d.dept_no JOIN (SELECT dm.dept_no, CONCAT(e.first_name, ' ', e.last_name) AS managers FROM employees AS e JOIN dept_manager AS dm ON e.emp_no = dm.emp_no	AND to_date > CURDATE()) AS m ON m.dept_no = d.dept_no ORDER BY d.dept_name;
+SELECT 
+    CONCAT(e.first_name, ' ', e.last_name) AS 'Employee Name',
+    d.dept_name AS 'Department Name',
+    m.managers AS 'Manager Name'
+FROM
+    employees AS e
+        JOIN
+    dept_emp AS de ON e.emp_no = de.emp_no
+        AND de.to_date > NOW()
+        JOIN
+    departments AS d ON de.dept_no = d.dept_no
+        JOIN
+    (SELECT 
+        dm.dept_no,
+            CONCAT(e.first_name, ' ', e.last_name) AS managers
+    FROM
+        employees AS e
+    JOIN dept_manager AS dm ON e.emp_no = dm.emp_no
+        AND to_date > CURDATE()) AS m ON m.dept_no = d.dept_no
+ORDER BY d.dept_name;
--- temporary_tables.sql
+++ temporary_tables.sql
@@ -1,11 +1,14 @@
 USE jemison_1741;
-SHOW CREATE DATABASE jemison_1741;
+DROP TABLE IF exists employees_with_departments;
 CREATE TEMPORARY TABLE employees_with_departments AS
 SELECT first_name, last_name, dept_name
 FROM employees.employees
 JOIN employees.dept_emp USING(emp_no)
 JOIN employees.departments USING(dept_no)
 LIMIT 100;
+
+# Test
+SELECT * FROM employees_with_departments;
 
 # a. Add a column named full_name to this table.
 # It should be a VARCHAR whose length is the sum of the lengths of the first name and last name columns
@@ -17,8 +20,36 @@ SHOW CREATE TABLE employees_with_departments;
   `last_name` varchar(16) CHARACTER SET latin1 NOT NULL,
   `dept_name` varchar(40) CHARACTER SET latin1 NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci' */
-ALTER TABLE employees_with_departments ADD COLUMN full_name VARCHAR(30);
+ALTER TABLE employees_with_departments ADD COLUMN full_name VARCHAR(31);
+
+# TEST
 SELECT * FROM employees_with_departments;
 
 # b. Update the table so that full name column contains the correct data
 UPDATE employees_with_departments SET full_name = CONCAT(first_name, ' ', last_name);
+
+# TEST
+SELECT * FROM employees_with_departments;
+
+# c. Remove the first_name and last_name columns from the table.
+ALTER TABLE employees_with_departments DROP COLUMN first_name;
+ALTER TABLE employees_with_departments DROP COLUMN last_name;
+
+# TEST
+SELECT * FROM employees_with_departments;
+
+# d. What is another way you could have ended up with this same table?
+# By running the following code:
+/* CREATE TEMPORARY TABLE employees_with_departments AS
+SELECT dept_name, CONCAT(first_name, last_name) AS full_name
+FROM employees.employees
+JOIN employees.dept_emp USING(emp_no)
+JOIN employees.departments USING(dept_no)
+LIMIT 100; */
+
+
+# 2. Create a temporary table based on the payment table from the sakila database.
+# Write the SQL necessary to transform the amount column such that it is stored as an integer representing the number of cents of the payment.
+# For example, 1.99 should become 199.
+
+CREATE TEMPORARY TABLE new_payment AS SELECT payment_id, dustomer_id, staff_id, rental_id, amount * 100 AS new_amount, payment_date, last_update FROM sakila.payment 
