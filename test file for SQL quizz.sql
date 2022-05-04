ad49a2f564a4e6ed5535e043f2219b628fdc69d1
Arsene Severin Boundaone <arsene.boundaone@gmail.com>
2022-05-04 05:02:07 -0500

    Adding some more SQL exercises for test file

--- test file for SQL quizz.sql
+++ test file for SQL quizz.sql
@@ -8,3 +8,4 @@ SELECT * FROM dept_emp;
 SELECT * FROM dept_emp WHERE emp_no < '10006' OR dept_no >= 'd005';
 SELECT distinct(first_name) FROM employees;
 SELECT CONCAT(first_name, ' ', last_name), SUBSTR("Krassimir Michaels", 12, 7) as NewName FROM employees WHERE first_name = 'Krassimir' AND last_name = 'Michaels';
+SELECT * FROM 
