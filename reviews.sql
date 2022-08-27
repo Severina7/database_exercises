e4984c8a114aaa0fcbd1b7aaec4033cf301f36f4
Arsene Severin Boundaone <arsene.boundaone@gmail.com>
2022-08-26 22:59:11 -0500

    adding more exercises

--- reviews.sql
+++ reviews.sql
@@ -5,3 +5,7 @@ SHOW DATABASES;
 USE saas_llc;
 SELECT database();
 SHOW CREATE DATABASE saas_llc;
+SHOW TABLES FROM saas_llc;
+SHOW COLUMNS FROM churn;
+SELECT COUNT(*) FROM churn;
+SELECT COUNT(*) FROM transactions;
