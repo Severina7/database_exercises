40b4c0dd9566374143b032a281390a788442936c
Arsene Severin Boundaone <arsene.boundaone@gmail.com>
2022-08-31 21:59:11 -0500

    adding more exercises

--- reviews.sql
+++ reviews.sql
@@ -9,3 +9,10 @@ SHOW TABLES FROM saas_llc;
 SHOW COLUMNS FROM churn;
 SELECT COUNT(*) FROM churn;
 SELECT COUNT(*) FROM transactions;
+USE employees;
+SHOW TABLES FROM employees;
+SHOW tables;
+SHOW CREATE table dept_manager;
+
+
+
--- tables_exercises.sql
+++ tables_exercises.sql
@@ -10,7 +10,7 @@ DESCRIBE departments;
 # Which table(s) do you think contain a numeric type column? All the table except departments contain at least a numeric type of column
 # Which table(s) do you think contain a string type column? All contain at least one string type of column but department is the only one to only have string type of column
 # Which table(s) do you think contain a date type column? All except the department table contain a date type column
-# What is the relationship between the employees and the departments tables? It is a one to many relationship because MySQL connects the elements of tables to each others in an almost infinite entanglement of links
+# What is the relationship between the employees and the departments tables? It is a one to many (from departments to employees) relationship because MySQL connects the elements of tables to each others in an almost infinite entanglement of links
 # Show the SQL that created the dept_manager table.
 SHOW CREATE TABLE dept_manager;
 'CREATE TABLE `dept_manager` (
