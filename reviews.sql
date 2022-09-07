25e64840d5bf10bc11b82ee24ccdae9f16195488
Arsene Severin Boundaone <arsene.boundaone@gmail.com>
2022-09-06 22:18:20 -0500

    adding more exercises

--- reviews.sql
+++ reviews.sql
@@ -49,3 +49,6 @@ SELECT birth_date, first_name, last_name FROM employees WHERE birth_date LIKE '%
 SELECT * FROM employees WHERE birth_date LIKE '%12-24' AND hire_date LIKE '199%';
 SELECT first_name, last_name FROM employees WHERE last_name LIKE '%q%';
 SELECT COUNT(*) FROM employees WHERE last_name LIKE '%q%' AND last_name NOT LIKE '%qu%';
+SELECT first_name, last_name FROM employees WHERE first_name IN ('Irena', 'Vidya', 'Maya') ORDER BY first_name;
+SELECT first_name, last_name FROM employees WHERE first_name IN ('Irena', 'Vidya', 'Maya') ORDER BY first_name LIMIT 1;
+SELECT first_name, last_name FROM employees WHERE first_name IN ('Irena', 'Vidya', 'Maya') ORDER BY first_name DESC;
