884cda64a5af13d0b535f80485a42b9afe1f032f
Arsene Severin Boundaone <arsene.boundaone@gmail.com>
2022-09-23 22:32:46 -0500

    more exercises

--- reviews.sql
+++ reviews.sql
@@ -566,3 +566,34 @@ WHERE
             to_date > CURDATE()));
             
 SELECT DATABASE();
+
+USE employees;
+SELECT e.emp_no, d.dept_no, e.first_name, e.last_name, e.hire_date, de.to_date,
+CASE 
+	WHEN to_date > CURDATE() THEN '1'
+    ELSE '0' 
+END AS is_current_employee
+FROM employees AS e
+JOIN dept_emp AS de ON e.emp_no = de.emp_no
+JOIN departments AS d USING(dept_no) LIMIT 50;
+
+USE employees;
+SELECT first_name, last_name,
+CASE
+	WHEN last_name LIKE 'A%' or last_name LIKE 'B%' or last_name LIKE 'C%' or last_name LIKE 'D%' or last_name LIKE 'E%' or last_name LIKE 'F%' or last_name LIKE 'G%' or last_name LIKE 'H%' THEN 'A-H'
+	WHEN last_name LIKE 'I%' or last_name LIKE 'J%' or last_name LIKE 'K%' or last_name LIKE 'L%' or last_name LIKE 'M%' or last_name LIKE 'N%' or last_name LIKE 'O%' or last_name LIKE 'P%' or last_name LIKE 'Q%' THEN 'I-Q'
+	WHEN last_name LIKE 'R%' or last_name LIKE 'S%' or last_name LIKE 'T%' or last_name LIKE 'U%' or last_name LIKE 'V%' or last_name LIKE 'W%' or last_name LIKE 'X%' or last_name LIKE 'Y%' or last_name LIKE 'Z%' THEN 'R-Z'
+    ELSE 'Other'
+END AS alpha_group
+FROM employees ORDER BY last_name;
+
+SELECT MAX(birth_date), MIN(birth_date) FROM employees;
+SELECT birth_date, 
+	COUNT(CASE WHEN birth_date LIKE '195%' THEN birth_date ELSE NULL END) AS Birthdates_in_50s,
+	COUNT(CASE WHEN birth_date LIKE '196%' THEN birth_date ELSE NULL END) AS Birthdates_in_60s
+FROM employees
+GROUP BY birth_date;
+
+SELECT * FROM departments;
+SELECT * FROM salaries;
+SELECT * 
